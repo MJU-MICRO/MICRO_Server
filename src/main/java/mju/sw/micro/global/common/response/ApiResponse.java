8665535e15c2975b5f1c066b6f6245462b2865e7
kimyubi <ieieie0419@gmail.co,=m>
2023-06-28 13:08:35 +0900

    Init: 공통 응답 클래스 ApiResponse.java 작성

---
+++ src/main/java/mju/sw/micro/global/common/response/ApiResponse.java
@@ -0,0 +1,40 @@
+package mju.sw.micro.global.common.response;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiResponse<T> {
+
+	// API 상태 코드
+	private int statusCode;
+
+	// API 상태
+	private HttpStatus status;
+
+	// API 응답 메시지
+	private String message;
+
+	// API 응답 데이터
+	private T data;
+
+	private ApiResponse(HttpStatus status, String message, T data) {
+		this.statusCode = status.value();
+		this.status = status;
+		this.message = message;
+		this.data = data;
+	}
+
+	public static <T> ApiResponse<T> of(HttpStatus status, String message, T data) {
+		return new ApiResponse<>(status, message, data);
+	}
+
+	public static <T> ApiResponse<T> of(HttpStatus status, T data) {
+		return of(status, status.name(), data);
+	}
+
+}
--- src/main/java/mju/sw/micro/global/config/jpa/JpaAuditingConfig.java
+++ src/main/java/mju/sw/micro/global/config/jpa/JpaAuditingConfig.java
@@ -1,4 +1,4 @@
-package mju.sw.micro.global.config;
+package mju.sw.micro.global.config.jpa;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
