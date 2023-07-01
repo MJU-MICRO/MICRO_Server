005f6b534d5dddc08c039cb0e9253e86bd883c30
kimyubi <ieieie0419@gmail.co,=m>
2023-07-01 23:55:09 +0900

    Init & Refactor : Jacoco 적용, 테스트 환경에서는 In Memory DB를 사용하도록 리팩토링

--- src/test/java/mju/sw/micro/ControllerTestSupporter.java
+++ src/test/java/mju/sw/micro/ControllerTestSupporter.java
@@ -3,6 +3,7 @@ package mju.sw.micro;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,6 +13,7 @@ import mju.sw.micro.domain.sample.application.SampleService;
 
 @WebMvcTest(controllers = {SampleController.class
 })
+@ActiveProfiles("test")
 public abstract class ControllerTestSupporter {
 
 	@MockBean
