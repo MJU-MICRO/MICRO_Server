74894c61dd3d3cdf1414e60b152245b673aa53c2
Changil Park <55132026+parkcoldroad@users.noreply.github.com>
2023-07-07 16:13:23 +0900

    refactor: Update class names from User to Auth

--- src/main/java/mju/sw/micro/domain/user/api/AuthApi.java
+++ src/main/java/mju/sw/micro/domain/user/api/AuthApi.java
@@ -3,7 +3,7 @@ package mju.sw.micro.domain.user.api;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import mju.sw.micro.domain.user.application.UserService;
+import mju.sw.micro.domain.user.application.AuthService;
 import mju.sw.micro.domain.user.dto.request.EmailSendRequestDto;
 import mju.sw.micro.domain.user.dto.request.SignUpRequestDto;
 import mju.sw.micro.domain.user.dto.request.CodeVerifyRequestDto;
@@ -16,26 +16,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "회원 API", description = "이메일 송신, 이메일 인증, 회원 가입, 로그인을 합니다.")
+@Tag(name = "인증 API", description = "이메일 송신, 이메일 인증, 회원 가입, 로그인을 합니다.")
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api")
 @RequiredArgsConstructor
-public class UserController {
+public class AuthApi {
 
-	private final UserService userService;
-	//TODO: 이메일과 관련된 컨트롤러, 서비스를 따로 만들까?
+	private final AuthService authService;
 	@Operation(summary = "이메일 송신 및 인증 코드 저장")
 	@PostMapping("/email")
 	@ResponseStatus(HttpStatus.OK)
 	public ApiResponse<String> sendEmailAndSaveCode(@Validated @RequestBody EmailSendRequestDto dto) {
-		return userService.sendEmailAndSaveCode(dto);
+		return authService.sendEmailAndSaveCode(dto);
 	}
 
 	@Operation(summary = "이메일 인증 코드 검증")
 	@PostMapping("/email/auth")
 	@ResponseStatus(HttpStatus.OK)
 	public ApiResponse<Boolean> verifyCode(@Validated @RequestBody CodeVerifyRequestDto dto) {
-		return userService.verifyCode(dto);
+		return authService.verifyCode(dto);
 	}
 
 	@Operation(summary = "회원가입")
@@ -43,7 +42,7 @@ public class UserController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public ApiResponse<String> signUp(
 		@Validated @RequestBody SignUpRequestDto dto) {
-		return userService.signUp(dto);
+		return authService.signUp(dto);
 		//TODO: s3 연동 후 이미지 업로드 기능 추가
 	}
 
--- src/main/java/mju/sw/micro/domain/user/application/AuthService.java
+++ src/main/java/mju/sw/micro/domain/user/application/AuthService.java
@@ -20,7 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class UserService {
+public class AuthService {
 
 	private final UserRepository userRepository;
 	private final MailUtil mailUtil;
