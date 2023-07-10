e5e5777f7dcd235940470bd4e93bf001e7a19d4a
Changil Park <55132026+parkcoldroad@users.noreply.github.com>
2023-07-11 00:24:32 +0900

    chore: Modify redis configuration

--- src/main/java/mju/sw/micro/global/config/RedisConfig.java
+++ src/main/java/mju/sw/micro/global/config/RedisConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -17,9 +19,16 @@ public class RedisConfig {
 	@Value("${spring.data.redis.port}")
 	private int port;
 
+	@Value("${spring.data.redis.password}")
+	private String password;
+
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory(host, port);
+		RedisStandaloneConfiguration redisConf = new RedisStandaloneConfiguration();
+		redisConf.setHostName(host);
+		redisConf.setPort(port);
+		redisConf.setPassword(RedisPassword.of(password));
+		return new LettuceConnectionFactory(redisConf);
 	}
 
 	@Bean
