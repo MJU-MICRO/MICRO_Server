8a8f0739c7c76fa61ed0ec4d50eb8026675ae7c2
parkcoldroad <coldstreet@naver.com>
2023-07-23 23:51:35 +0900

    feat: Add s3 uploader

--- src/main/java/mju/sw/micro/domain/user/application/AuthService.java
+++ src/main/java/mju/sw/micro/domain/user/application/AuthService.java
@@ -5,8 +5,10 @@ import java.util.Optional;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import mju.sw.micro.domain.user.dao.EmailCodeRedisRepository;
 import mju.sw.micro.domain.user.dao.RefreshTokenRedisRepository;
 import mju.sw.micro.domain.user.dao.UserRepository;
@@ -22,6 +24,7 @@ import mju.sw.micro.domain.user.dto.request.SignUpRequestDto;
 import mju.sw.micro.domain.user.dto.response.LoginResponseDto;
 import mju.sw.micro.domain.user.dto.response.RefreshTokenResponseDto;
 import mju.sw.micro.global.adapter.MailService;
+import mju.sw.micro.global.adapter.S3Uploader;
 import mju.sw.micro.global.common.response.ApiResponse;
 import mju.sw.micro.global.constants.EmailConstants;
 import mju.sw.micro.global.constants.JwtConstants;
@@ -33,6 +36,7 @@ import mju.sw.micro.global.utils.TimeUtil;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
+@Slf4j
 public class AuthService {
 
 	private final UserRepository userRepository;
@@ -41,6 +45,7 @@ public class AuthService {
 	private final EmailCodeRedisRepository emailCodeRedisRepository;
 	private final PasswordEncoder encoder;
 	private final JwtService jwtService;
+	private final S3Uploader s3Uploader;
 
 	public ApiResponse<String> sendEmailAndSaveCode(EmailSendRequestDto dto) {
 		String emailCode = CodeUtil.generateRandomCode();
@@ -63,7 +68,7 @@ public class AuthService {
 	}
 
 	@Transactional
-	public ApiResponse<String> signUp(SignUpRequestDto dto) {
+	public ApiResponse<String> signUp(SignUpRequestDto dto, MultipartFile imageFile) {
 		if (userRepository.findByEmail(dto.getEmail()).isPresent()) {
 			return ApiResponse.withError(ErrorCode.ALREADY_SIGN_UP_EMAIL);
 		}
@@ -71,7 +76,7 @@ public class AuthService {
 		if (!isVerified) {
 			return ApiResponse.withError(ErrorCode.INVALID_TOKEN);
 		}
-
+		String imageUrl = uploadImage(imageFile).getData();
 		User user = User.builder()
 			.email(dto.getEmail())
 			.password(encoder.encode(dto.getPassword()))
@@ -83,6 +88,7 @@ public class AuthService {
 			.phoneNumber(dto.getPhoneNumber())
 			.introduction(dto.getIntroduction())
 			.notification(dto.getNotification())
+			.imageUrl(imageUrl)
 			.build();
 		user.addRole(Role.ROLE_USER);
 		userRepository.save(user);
@@ -122,5 +128,12 @@ public class AuthService {
 			RefreshToken.of(refreshToken.getEmail(), JwtConstants.REFRESH_TOKEN_EXPIRATION_TIME,
 				reIssuedRefreshToken, TimeUtil.generateExpiration(JwtConstants.REFRESH_TOKEN_EXPIRATION_TIME)));
 		return ApiResponse.ok("토큰 재발급에 성공했습니다.", RefreshTokenResponseDto.of(reIssuedAccessToken, reIssuedRefreshToken));
+	}
+
+	private ApiResponse<String> uploadImage(MultipartFile multipartFile) {
+		if (multipartFile == null) {
+			return ApiResponse.ok("이미지가 없습니다", null);
+		}
+		return s3Uploader.uploadFile(multipartFile);
 	}
 }
