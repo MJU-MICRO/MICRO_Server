e31a58e763c26034e6c945d849dc7f1e22eaf101
Changil Park <55132026+parkcoldroad@users.noreply.github.com>
2023-08-07 18:48:31 +0900

    feat: Add sample api (#35)

--- src/main/java/mju/sw/micro/domain/sample/api/SampleApi.java
+++ src/main/java/mju/sw/micro/domain/sample/api/SampleApi.java
@@ -1,30 +1,18 @@
 package mju.sw.micro.domain.sample.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import mju.sw.micro.domain.sample.application.SampleService;
-import mju.sw.micro.domain.sample.domain.Sample;
-import mju.sw.micro.domain.sample.dto.request.SampleCreateRequest;
-import mju.sw.micro.global.common.response.ApiResponse;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/sample")
 @RequiredArgsConstructor
 public class SampleApi {
 
 	private final SampleService sampleService;
-
-	@PostMapping("/sample")
-	public ApiResponse<Sample> createSample(@Valid @RequestBody SampleCreateRequest request) {
-		Sample savedSample = sampleService.createSample(request.toServiceRequest());
-		return ApiResponse.ok(savedSample);
-	}
 
 	@GetMapping("/hello")
 	public String hello() {
