85ee90e91626c773fa21cce3263d3a4732065c2b
parkcoldroad <coldstreet@naver.com>
2023-08-12 01:03:05 +0900

    test: Add deleteUserByAdmin test

--- src/test/java/mju/sw/micro/domain/admin/application/AdminServiceTest.java
+++ src/test/java/mju/sw/micro/domain/admin/application/AdminServiceTest.java
@@ -1,5 +1,7 @@
 package mju.sw.micro.domain.admin.application;
 
+import java.util.Optional;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -20,7 +22,7 @@ class AdminServiceTest extends IntegrationTestSupporter {
 		userRepository.deleteAll();
 	}
 
-	@DisplayName("관리자를 등록한다")
+	@DisplayName("관리자 권한을 등록한다")
 	@Test
 	void registerAdmin() {
 		// given
@@ -32,10 +34,10 @@ class AdminServiceTest extends IntegrationTestSupporter {
 		User updatedUser = userRepository.findByEmail(MockConstants.MOCK_USER_EMAIL).get();
 		// then
 		Assertions.assertThat(updatedUser.isAdmin()).isTrue();
-		Assertions.assertThat(userResponse.getMessage()).isEqualTo("관리자 등록에 성공했습니다.");
+		Assertions.assertThat(userResponse.getMessage()).isEqualTo("관리자 권한 등록에 성공했습니다.");
 	}
 
-	@DisplayName("존재하지 않는 이메일을 입력하여 관리자 등록을 실패한다")
+	@DisplayName("존재하지 않는 이메일을 입력하여 관리자 권한 등록을 실패한다")
 	@Test
 	void registerAdminWithInvalidEmail() {
 		// given
@@ -48,7 +50,7 @@ class AdminServiceTest extends IntegrationTestSupporter {
 		Assertions.assertThat(userResponse.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
 	}
 
-	@DisplayName("관리자를 해지한다")
+	@DisplayName("관리자 권한을 해지한다")
 	@Test
 	void revokeAdmin() {
 		// given
@@ -60,10 +62,10 @@ class AdminServiceTest extends IntegrationTestSupporter {
 		User updatedUser = userRepository.findByEmail(MockConstants.MOCK_USER_EMAIL).get();
 		// then
 		Assertions.assertThat(updatedUser.isAdmin()).isFalse();
-		Assertions.assertThat(userResponse.getMessage()).isEqualTo("관리자 해지에 성공했습니다.");
+		Assertions.assertThat(userResponse.getMessage()).isEqualTo("관리자 권한 해지에 성공했습니다.");
 	}
 
-	@DisplayName("존재하지 않는 이메일을 입력하여 관리자 해지를 실패한다")
+	@DisplayName("존재하지 않는 이메일을 입력하여 관리자 권한 해지를 실패한다")
 	@Test
 	void revokeAdminWithInvalidEmail() {
 		// given
@@ -76,4 +78,31 @@ class AdminServiceTest extends IntegrationTestSupporter {
 		Assertions.assertThat(userResponse.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
 	}
 
+	@DisplayName("관리자가 계정을 삭제한다")
+	@Test
+	void deleteUserByAdmin() {
+		// given
+		User user = MockFactory.createMockUser();
+		user.addRole(Role.ROLE_USER);
+		userRepository.save(user);
+		//when
+		ApiResponse<Void> userResponse = adminService.deleteUserByAdmin(user.getEmail());
+		Optional<User> optionalUser = userRepository.findByEmail(user.getEmail());
+		// then
+		Assertions.assertThat(userResponse.getMessage()).isEqualTo("관리자가 회원을 삭제했습니다.");
+		Assertions.assertThat(optionalUser).isEmpty();
+	}
+
+	@DisplayName("존재하지 않는 이메일을 입력하여 관리자가 계정 삭제를 실패한다")
+	@Test
+	void deleteUserByAdminWithInvalidEmail() {
+		// given
+		User user = MockFactory.createMockUser();
+		user.addRole(Role.ROLE_USER);
+		userRepository.save(user);
+		//when
+		ApiResponse<Void> userResponse = adminService.deleteUserByAdmin("invalidEmail");
+		// then
+		Assertions.assertThat(userResponse.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
+	}
 }
