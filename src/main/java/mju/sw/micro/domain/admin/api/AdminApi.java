d819af0c89a2ac0b99cb76e79f786c77bd8c9f3b
parkcoldroad <coldstreet@naver.com>
2023-08-12 01:03:43 +0900

    feat: Add deleteUserByAdmin api

--- src/main/java/mju/sw/micro/domain/admin/api/AdminApi.java
+++ src/main/java/mju/sw/micro/domain/admin/api/AdminApi.java
@@ -2,7 +2,8 @@ package mju.sw.micro.domain.admin.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,17 +20,24 @@ import mju.sw.micro.global.common.response.ApiResponse;
 public class AdminApi {
 	private final AdminService adminService;
 
-	@Operation(summary = "관리자 등록")
-	@PostMapping("/register")
+	@Operation(summary = "관리자 권한 부여")
+	@PatchMapping("/register")
 	@ResponseStatus(HttpStatus.CREATED)
 	public ApiResponse<Void> registerAdmin(@Validated AdminRequestDto dto) {
 		return adminService.registerAdmin(dto.getEmail());
 	}
 
-	@Operation(summary = "관리자 해지")
-	@PostMapping("/revoke")
+	@Operation(summary = "관리자 권한 해지")
+	@PatchMapping("/revoke")
 	@ResponseStatus(HttpStatus.OK)
 	public ApiResponse<Void> revokeAdmin(@Validated AdminRequestDto dto) {
 		return adminService.revokeAdmin(dto.getEmail());
+	}
+
+	@Operation(summary = "관리자 권한으로 계정 삭제")
+	@DeleteMapping("/user/delete")
+	@ResponseStatus(HttpStatus.OK)
+	public ApiResponse<Void> deleteUserByAdmin(@Validated AdminRequestDto dto) {
+		return adminService.deleteUserByAdmin(dto.getEmail());
 	}
 }
