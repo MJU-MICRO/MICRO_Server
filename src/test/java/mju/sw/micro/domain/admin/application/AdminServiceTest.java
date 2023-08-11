d2f7eb31650bc497c7f1286206f88f78aee54229
parkcoldroad <coldstreet@naver.com>
2023-08-12 00:27:33 +0900

    test: Add revokeAdmin test

--- src/test/java/mju/sw/micro/domain/admin/application/AdminServiceTest.java
+++ src/test/java/mju/sw/micro/domain/admin/application/AdminServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
 import mju.sw.micro.IntegrationTestSupporter;
+import mju.sw.micro.domain.user.domain.Role;
 import mju.sw.micro.domain.user.domain.User;
 import mju.sw.micro.global.common.response.ApiResponse;
 import mju.sw.micro.global.utils.MockConstants;
@@ -24,6 +25,7 @@ class AdminServiceTest extends IntegrationTestSupporter {
 	void registerAdmin() {
 		// given
 		User user = MockFactory.createMockUser();
+		user.addRole(Role.ROLE_USER);
 		userRepository.save(user);
 		// when
 		ApiResponse<Void> userResponse = adminService.registerAdmin(user.getEmail());
@@ -33,15 +35,45 @@ class AdminServiceTest extends IntegrationTestSupporter {
 		Assertions.assertThat(userResponse.getMessage()).isEqualTo("관리자 등록에 성공했습니다.");
 	}
 
-	@DisplayName("관리자를 등록한다")
+	@DisplayName("존재하지 않는 이메일을 입력하여 관리자 등록을 실패한다")
 	@Test
 	void registerAdminWithInvalidEmail() {
 		// given
 		User user = MockFactory.createMockUser();
+		user.addRole(Role.ROLE_USER);
 		userRepository.save(user);
 		// when
 		ApiResponse<Void> userResponse = adminService.registerAdmin("invalidEmail");
 		// then
 		Assertions.assertThat(userResponse.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
 	}
+
+	@DisplayName("관리자를 해지한다")
+	@Test
+	void revokeAdmin() {
+		// given
+		User user = MockFactory.createMockUser();
+		user.addRole(Role.ROLE_ADMIN);
+		userRepository.save(user);
+		// when
+		ApiResponse<Void> userResponse = adminService.revokeAdmin(user.getEmail());
+		User updatedUser = userRepository.findByEmail(MockConstants.MOCK_USER_EMAIL).get();
+		// then
+		Assertions.assertThat(updatedUser.isAdmin()).isFalse();
+		Assertions.assertThat(userResponse.getMessage()).isEqualTo("관리자 해지에 성공했습니다.");
+	}
+
+	@DisplayName("존재하지 않는 이메일을 입력하여 관리자 해지를 실패한다")
+	@Test
+	void revokeAdminWithInvalidEmail() {
+		// given
+		User user = MockFactory.createMockUser();
+		user.addRole(Role.ROLE_ADMIN);
+		userRepository.save(user);
+		// when
+		ApiResponse<Void> userResponse = adminService.revokeAdmin("invalidEmail");
+		// then
+		Assertions.assertThat(userResponse.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
+	}
+
 }
