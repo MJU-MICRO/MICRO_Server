03c04507fabee7bffcebbd3a91bd614e8a852a5a
parkcoldroad <coldstreet@naver.com>
2023-08-12 00:16:35 +0900

    feat: Add registerAdmin function

---
+++ src/main/java/mju/sw/micro/domain/admin/application/AdminService.java
@@ -0,0 +1,32 @@
+package mju.sw.micro.domain.admin.application;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import mju.sw.micro.domain.user.dao.UserRepository;
+import mju.sw.micro.domain.user.domain.Role;
+import mju.sw.micro.domain.user.domain.User;
+import mju.sw.micro.global.common.response.ApiResponse;
+import mju.sw.micro.global.error.exception.ErrorCode;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AdminService {
+
+	private final UserRepository userRepository;
+
+	@Transactional
+	public ApiResponse<Void> registerAdmin(String email) {
+		Optional<User> optionalUser = userRepository.findByEmail(email);
+		if (optionalUser.isEmpty()) {
+			return ApiResponse.withError(ErrorCode.NOT_FOUND);
+		}
+		User user = optionalUser.get();
+		user.addRole(Role.ROLE_ADMIN);
+		return ApiResponse.ok("관리자 등록에 성공했습니다.");
+	}
+}
