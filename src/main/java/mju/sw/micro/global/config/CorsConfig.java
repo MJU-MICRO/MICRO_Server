a80e448eab98f577f49b7fe3f6df64b041dbd691
parkcoldroad <coldstreet@naver.com>
2023-08-19 15:04:11 +0900

    chore: Add cors config of React

--- src/main/java/mju/sw/micro/global/config/CorsConfig.java
+++ src/main/java/mju/sw/micro/global/config/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig {
 		CorsConfiguration config = new CorsConfiguration();
 
 		config.setAllowCredentials(true);
-		config.setAllowedOrigins(List.of(originUrl));
+		config.setAllowedOrigins(List.of(originUrl, "http://localhost:3000"));
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
 		config.setAllowedHeaders(List.of("*"));
 		config.setExposedHeaders(List.of("*"));
