7bc83b165e338ec7a6993e6f2e341f84b8f77d2e
Changil Park <55132026+parkcoldroad@users.noreply.github.com>
2023-08-20 16:48:32 +0900

    관리자 CRUD (#43)
    * feat: Add DBInitconfiguration
    
    * feat: Add registerAdmin function
    
    * test: Add registerAdmin test
    
    * feat: Add registerAdmin api
    
    * feat: Add revokeAdmin function
    
    * test: Add revokeAdmin test
    
    * feat: Add revokeAdmin api
    
    * feat: Add deleteUserByAdmin function
    
    * test: Add deleteUserByAdmin test
    
    * feat: Add deleteUserByAdmin api
    
    * feat: Add permission-based exception handling
    
    * test: Add permission-based exception handling test
    
    * feat: Add getUsersByAdminRole function
    
    * test: Add getUsersByAdminRole test
    
    * feat: Add getUsersByAdminRole api
    
    * feat: Add getUsersWithoutAdminRole function
    
    * test: Add getUsersWithoutAdminRole test
    
    * feat: Add getUsersWithoutAdminRole api
    
    * feat: Add exception to prevent admin from revoking myself
    
    * test: Add test to prevent admin from revoking myself
    
    * feat: Add exception to prevent admin from deleting myself
    
    * test: Add test to prevent admin from deleting myself
    
    * refactor: Modify ResponseStatus in api
    
    * refactor: Modify getUsersByAdminRole return type to using dto
    
    * test: Modify getUsersByAdminRole test to using dto
    
    * feat: Modify getUsersWithoutAdminRole return type to using dto
    
    * test: Add getUsersWithoutAdminRole test

--- src/main/java/mju/sw/micro/domain/user/domain/User.java
+++ src/main/java/mju/sw/micro/domain/user/domain/User.java
@@ -4,6 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -106,8 +115,7 @@ public class User extends BaseEntity {
 	}
 
 	public boolean isAdmin() {
-		return userRoles.stream()
-			.anyMatch(userRole -> userRole.getRole().equals(Role.ROLE_ADMIN));
+		return userRoles.stream().anyMatch(userRole -> userRole.getRole().equals(Role.ROLE_ADMIN));
 	}
 
 	public void setBookmark(List<String> collect) {
