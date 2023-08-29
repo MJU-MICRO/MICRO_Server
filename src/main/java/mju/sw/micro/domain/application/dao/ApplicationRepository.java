e814af5d7265c2157f66bfedfcf4c14f0a7f9744
parkcoldroad <coldstreet@naver.com>
2023-08-29 19:26:39 +0900

    feat: Add exception for applying to the same recruitment

--- src/main/java/mju/sw/micro/domain/application/dao/ApplicationRepository.java
+++ src/main/java/mju/sw/micro/domain/application/dao/ApplicationRepository.java
@@ -1,6 +1,7 @@
 package mju.sw.micro.domain.application.dao;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,5 +12,5 @@ public interface ApplicationRepository extends JpaRepository<Application, Long> 
 
 	List<Application> findByRecruitmentId(Long recruitmentId);
 
-	boolean existsByUserIdAndRecruitmentId(Long userId, Long recruitmentId);
+	Optional<Application> findByUserIdAndRecruitmentId(Long userId, Long recruitmentId);
 }
