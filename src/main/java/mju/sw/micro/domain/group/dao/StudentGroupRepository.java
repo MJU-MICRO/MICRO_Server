efe196f753cce1b7556b8876ba632b3887ba06ad
parkcoldroad <coldstreet@naver.com>
2023-08-29 20:52:44 +0900

    refactor: 회장이 확인할 신청서 조회 로직 수정

--- src/main/java/mju/sw/micro/domain/group/dao/StudentGroupRepository.java
+++ src/main/java/mju/sw/micro/domain/group/dao/StudentGroupRepository.java
@@ -1,14 +1,13 @@
 package mju.sw.micro.domain.group.dao;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import mju.sw.micro.domain.group.domain.StudentGroup;
 
 public interface StudentGroupRepository extends JpaRepository<StudentGroup, Long> {
-	Optional<StudentGroup> findByPresidentId(Long userId);
+	List<StudentGroup> findAllByPresidentId(Long userId);
 
 	List<StudentGroup> findByIsRecruitTrue();
 }
